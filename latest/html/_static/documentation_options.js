9c853fb689d98862d46173a42161c73f633edf38
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-18 12:02:46 +0000

    Deploy gwastro/pycbc to github.com/gwastro/pycbc.git:gh-pages

--- latest/html/_static/documentation_options.js
+++ latest/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '73d1b7',
+    VERSION: '1.14.dev1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
